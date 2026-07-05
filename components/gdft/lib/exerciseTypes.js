8819b858d2345bb2c8391156329911c7c78fe490
RickB54 <rberube54@gmail.com>
2026-07-04 22:58:31 -0400

    Add remaining untracked js build files

---
+++ components/gdft/lib/exerciseTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EQUIPMENT_OPTIONS = void 0;
+exports.EQUIPMENT_OPTIONS = [
+    "All",
+    "Barbell",
+    "Dumbbells",
+    "Bodyweight",
+    "Machine",
+    "Kettlebells",
+    "Resistance Bands",
+    "Cable",
+    "Slide Board",
+    "Treadmill",
+    "Bike",
+    "Stationary Bike",
+    "Rower",
+    "Rowing Machine",
+    "Elliptical",
+    "Jump Rope",
+    "Pull-Up Bar",
+    "Bench",
+    "Medicine Ball",
+    "Stair Climber",
+    "Box",
+    "None"
+];
+// Remove this line as these types are already exported above
+// export type { ExerciseCategory, MuscleGroup, Equipment };
